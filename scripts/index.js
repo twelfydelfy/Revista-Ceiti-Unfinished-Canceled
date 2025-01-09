9846c42c6f5b41529cc44e1d4b97282f004e571d
John Doe <fortunadanielmd@gmail.com>
2025-01-09 15:33:08 +0200

    added the final animation needed

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
 let currentslide = 1;
 
+let currentbox = 1;
+
 const overflow = document.querySelector('.overflow');
 const midgrid = document.querySelector('.midgrid');
 const slider = document.querySelector('.slider');
 const dot1 = document.querySelector('.dot1');
 const dot2 = document.querySelector('.dot2');
 const dot3 = document.querySelector('.dot3');
+const overflow2 = document.querySelector('.overflow2');
 
 document.querySelector('.leftbtn').addEventListener('click', () =>{
     console.log('clicked');
@@ -44,3 +47,27 @@ function dotscolor(){
         dot3.style.opacity = '1';
     }
 }
+
+document.querySelectorAll('.box2button').forEach((button) =>{
+    button.addEventListener('click', ()=>{
+        if(currentbox == 1){
+            overflow2.style.marginLeft = '-100%';
+            currentbox = 2;
+            document.querySelector('.box21').classList.remove('active');
+            document.querySelector('.box22').classList.add('active');
+        }
+        else if(currentbox == 2){
+            overflow2.style.marginLeft = '-200%';
+            currentbox = 3;
+            document.querySelector('.box22').classList.remove('active');
+            document.querySelector('.box23').classList.add('active');
+        }
+        else if(currentbox == 3){
+            overflow2.style.marginLeft = '0%';
+            currentbox = 1;
+            console.log(currentbox);
+            document.querySelector('.box23').classList.remove('active');
+            document.querySelector('.box21').classList.add('active');
+        }
+    });
+});
